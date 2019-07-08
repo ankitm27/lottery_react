ec7fdc923ac56c7759bf12dbe9de297e3d02bb59
ankitm27 <anki.malhotra00@gmail.com>
2019-07-08 16:19:40 +0530

    lottery code added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import web3 from './web3.js';
+import lottery from "./lottery.js";
 
-function App() {
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      manager:""
+    }
+  }
+  async componentDidMount (){
+    const manager = await lottery.methods.manager().call();
+    this.setState({manager:manager})
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>lottery contract</h1>
+      <p>this contract is manager by {this.state.manager} </p>
     </div>
   );
+  }
 }
 
 export default App;
